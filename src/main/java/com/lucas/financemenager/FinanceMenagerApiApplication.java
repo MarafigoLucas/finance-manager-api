024f1e9e6fe277389c7b13edab1b3d11f2427d11
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-01-22 22:55:49 -0300

    chore: initial Spring Boot setup and base package structure

---
+++ src/main/java/com/lucas/financemenager/FinanceMenagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.financemenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMenagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMenagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/financemenager/FinanceMenagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.financemenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMenagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
